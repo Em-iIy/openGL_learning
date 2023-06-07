e5f90eb5b309bebcfb7316d8ac62cb3b5e5b9087
Glenn Winnink <gwinnink@student.codam.nl>
2023-06-07 18:53:56 +0200

    added vector constructor

--- EBO.cpp
+++ EBO.cpp
@@ -4,11 +4,25 @@ EBO::EBO(void)
 {
 }
 // Constructor that generates a Elements Buffer Object and links it to indices
+EBO::EBO(std::vector<uint> &indices)
+{
+	glGenBuffers(1, &ID);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ID);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(uint), indices.data(), GL_STATIC_DRAW);
+}
+
 EBO::EBO(GLuint* indices, GLsizeiptr size)
 {
 	glGenBuffers(1, &ID);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ID);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, indices, GL_STATIC_DRAW);
+}
+
+
+void EBO::Update(std::vector<uint> &indices)
+{
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ID);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(uint), indices.data(), GL_STATIC_DRAW);
 }
 
 // Binds the EBO
--- EBO.hpp
+++ EBO.hpp
@@ -2,6 +2,8 @@
 #define EBO_CLASS_H
 
 #include <GLAD/glad.h>
+#include <vector>
+#include <sys/types.h>
 
 class EBO
 {
@@ -12,7 +14,8 @@ public:
 	EBO(void);
 	// Constructor that generates a Elements Buffer Object and links it to indices
 	EBO(GLuint* indices, GLsizeiptr size);
-
+	EBO(std::vector<uint> &indices);
+	void Update(std::vector<uint> &indices);
 	// Binds the EBO
 	void Bind();
 	// Unbinds the EBO
